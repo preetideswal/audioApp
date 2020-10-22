abb3e4bee58e39ca508217a9877225525fb8c575
preetideswal <61753161+preetideswal@users.noreply.github.com>
2020-10-22 22:19:05 +0530

    main file

---
+++ main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:My_App/ui/code.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AudioScreen();
+  }
+}
